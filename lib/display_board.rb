bc0d07b34a82ea4b4dd9d46a329f2c44419c22df
Chen Ye <dpp0921@gmail.com>
2018-01-16 22:16:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 b = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
+def display_board(b)
   puts " #{b[0]} / #{b[1]} / #{b[2]} "
   puts "-----------"
   puts " #{b[3]} / #{b[4]} / #{b[5]} "
